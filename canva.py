371eb2a8ccfdaa938e745d27eeb3c375a666531f
Ayrtonguimaraes <131915450+Ayrtonguimaraes@users.noreply.github.com>
2023-04-29 20:29:28 -0300

    Update canva.py

--- canva.py
+++ canva.py
@@ -1,1 +1,17 @@
+import pygame
 
+#inicia o pygame
+pygame.init()
+
+#Cria a tela do jogo
+tela = pygame.display.set_mode((800, 600))
+
+#Título e ícone do jogo
+pygame.display.set_caption("Nome jogo")
+#icon = pygame.image.load("iconejogo.png")
+
+jogo_rodando = True
+while jogo_rodando:
+    for evento in pygame.event.get():
+        if evento.type == pygame.QUIT:
+            jogo_rodando == False
