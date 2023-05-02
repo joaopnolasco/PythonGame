bc04e6b616802ce19f408780f4e1fb3749b30b29
Ayrtonguimaraes <131915450+Ayrtonguimaraes@users.noreply.github.com>
2023-05-02 20:20:35 -0300

    Create trofeu.py

---
+++ trofeu.py
@@ -0,0 +1,23 @@
+import pygame as pg
+import random
+
+imgTrofeu = pg.image.load("trophy.png")
+
+class Trofeu:
+    def __init__(self, win, x, y, largura, altura, vel, cor):
+        self.win = win
+        self.x = x
+        self.y = y
+        self.largura = largura
+        self.altura = altura
+        self.vel = vel
+        self.cor = cor
+
+    def draw(self):
+        self.win.blit(imgTrofeu, (self.x, self.y))
+
+    def move(self):
+        self.y += self.vel
+        if self.y > 600:
+            self.x = random.randint(0, 800)
+            self.y = -self.altura
